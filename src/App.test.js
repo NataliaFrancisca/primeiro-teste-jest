b85b8562894d1bc0a75a53d4a97038749419461a
NataliaFrancisca <nathifrancisca@gmail.com>
2022-04-11 11:22:45 -0300

    mudando as descrições para inglês

--- src/App.test.js
+++ src/App.test.js
@@ -1,52 +1,49 @@
-// ✔ Quando a tela carrega, o componente renderiza corretamente
-// ✔ A imagem está acessível com o texto alternativo?
-// ✔ O input funciona capturando o valor digitado?
-// ✔ Após a usuária digitar o nome, o texto renderiza corretamente na tela?
-
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('deverá renderizar os elementos corretamente', () => {
-  render(<App/>)
-  const textElement = screen.queryByText('Hello,');
-  expect(textElement).toBeInTheDocument();
-
-  const inputElement = screen.queryByPlaceholderText('digite seu nome');
-  expect(inputElement).toBeInTheDocument();
-
-  const imageElement = screen.queryByRole('img');
-  expect(imageElement).toBeInTheDocument();
-})
-
-test('Deverá encontrar a imagem com o texto alternativo correto', () => {
-  render(<App />)
-  const altTextImageElement  = screen.queryByAltText("ilustração de uma mulher negra usando o computador e segurando uma xícara");
-  expect(altTextImageElement).toBeInTheDocument();
-})
-
-test('Deverá pegar o input corretamente', () => {
-  render(<App />)
-  const inputElement = screen.queryByPlaceholderText("digite seu nome");
-  fireEvent.change(inputElement, {
-    target: {
-      value: 'Simara'
-    }
+describe("App", () => {
+  it('should be able to render the component correctly', () => {
+    render(<App/>)
+    const textElement = screen.queryByText('Hello,');
+    expect(textElement).toBeInTheDocument();
+  
+    const inputElement = screen.queryByPlaceholderText('digite seu nome');
+    expect(inputElement).toBeInTheDocument();
+  
+    const imageElement = screen.queryByRole('img');
+    expect(imageElement).toBeInTheDocument();
   })
-
-  const valorDigitado = screen.queryByText("Simara");
-  expect(valorDigitado).toHaveTextContent("Simara");
-})
-
-test('Deverá renderizar corretamente o texto digitado', () => {
-  render(<App />)
-
-  const inputElement = screen.queryByPlaceholderText("digite seu nome");
-  fireEvent.change(inputElement, {
-    target: {
-      value: 'Simara'
-    }
+  
+  it('should be able to find the image by the alt text', () => {
+    render(<App />)
+    const altTextImageElement  = screen.queryByAltText("ilustração de uma mulher negra usando o computador e segurando uma xícara");
+    expect(altTextImageElement).toBeInTheDocument();
   })
-
-  expect(screen.queryByText('Simara')).toHaveTextContent("Simara");
-  expect(screen.queryByText('Hello,')).toHaveTextContent("Hello, Simara");
+  
+  it('should be able to get input value correctly', () => {
+    render(<App />)
+    const inputElement = screen.queryByPlaceholderText("digite seu nome");
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'Simara'
+      }
+    })
+  
+    const valorDigitado = screen.queryByText("Simara");
+    expect(valorDigitado).toHaveTextContent("Simara");
+  })
+  
+  it('should be able to render the correct content after user type', () => {
+    render(<App />)
+  
+    const inputElement = screen.queryByPlaceholderText("digite seu nome");
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'Simara'
+      }
+    })
+  
+    expect(screen.queryByText('Simara')).toHaveTextContent("Simara");
+    expect(screen.queryByText('Hello,')).toHaveTextContent("Hello, Simara");
+  })
 })
